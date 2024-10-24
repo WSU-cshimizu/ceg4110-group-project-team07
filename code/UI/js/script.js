d8f6cc9d5ec9e4d0baef9f60bd99fd2457bf101e
Anna-Crafton <129332834+Anna-Crafton@users.noreply.github.com>
2024-10-24 15:00:56 -0400

    Update script.js
    removed open_popup() and close_Popup(), which I am no longer using since the upload popup has been changed to it's own page.
    
    added setDisplay() functions to change the location displays on view page of the website. They currently only fill in with dummy information.

--- code/UI/js/script.js
+++ code/UI/js/script.js
@@ -2,7 +2,7 @@
     script.js (javascript for GeoSyncra website)
     ceg411
     Anna Crafton
-    last updated 10/3/2024
+    last updated 10/24/2024
 
     
     ToDo Specifications:
@@ -10,21 +10,45 @@
     
 */
 
-/*  openPopup()
-    funcntion for opening a popup form
-    takes no input
-    returns nothing
-    no known bugs*/
-function openPopup() {
-    document.getElementById("uploadPopup").style.display = "block";
- }
+/*
+  setDisplay_1()
+  Function to change the display elements of viewpage locations
+  Can replace vars with info from database later. Maybe make a class? FLAG!*/
+    function setDisplay_1(){
+        var locationName = "Location_Name";
+        var locationDescription = "Location_Description"
+        var locationImage = "photos/dummyPhoto.jpg";
+    
+        document.getElementById("location_Name_1").textContent=locationName;
+        document.getElementById("location_Description_1").textContent=locationDescription;
+        document.getElementById("location_Image_1").src=locationImage;
 
+    }
+   
+/*setDisplay_2() 
+  Same as setDisplay_1, but for the second display. 
+  This is unessisary becasue there is deffinitly a way to do this that loops through all displays, and I need to fix it.*/
+  function setDisplay_2(){
+    var locationName = "Location_Name";
+    var locationDescription = "Location_Description"
+    var locationImage = "photos/dummyPhoto.jpg";
 
-/*  closePopup()
-    funcntion for closing a popup form
-    takes no input
-    returns nothing
-    no known bugs */
-function closePopup() {
-    document.getElementById("uploadPopup").style.display = "none";
- }
+    document.getElementById("location_Name_2").textContent=locationName;
+    document.getElementById("location_Description_2").textContent=locationDescription;
+    document.getElementById("location_Image_2").src=locationImage;
+
+}
+
+   
+/*setDisplay_3()
+  see setDIsplay_2()*/
+  function setDisplay_3(){
+    var locationName = "Location_Name";
+    var locationDescription = "Location_Description"
+    var locationImage = "photos/dummyPhoto.jpg";
+
+    document.getElementById("location_Name_3").textContent=locationName;
+    document.getElementById("location_Description_3").textContent=locationDescription;
+    document.getElementById("location_Image_3").src=locationImage;
+
+}
